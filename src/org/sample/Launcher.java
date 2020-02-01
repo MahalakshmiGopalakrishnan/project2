3a4d974e466876ddb96a956398604e513ea9e859
MahalakshmiGopalakrishnan <mahalakshmiee31@gmail.com>
2020-02-01 21:53:10 +0530

    login functionality code done

---
+++ src/org/sample/Launcher.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+public class Launcher {
+	
+		public static void main(String[] args) {
+			String s="Mahathi";
+			int le=s.length();
+			System.out.println(le);
+			char c= s.charAt(5);
+		}
+
+	}
+
+
+
